127cdd8135a5cea96384ef35bf4b1366f1d94877
dustinober1 <dober.code@gmail.com>
2026-02-22 21:08:52 -0500

    fix: add missing go_router import in sign_in_screen.dart

--- flutter_app/lib/features/auth/presentation/sign_in_screen.dart
+++ flutter_app/lib/features/auth/presentation/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../app/theme.dart';
 import '../providers.dart';
