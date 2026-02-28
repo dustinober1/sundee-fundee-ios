3ad8d649858ac1f965d8751d85515c8dfa8219b3
dustinober1 <dober.code@gmail.com>
2026-02-28 15:04:43 -0500

    refactor: Remove deprecated AppSchemaV2, V3, V4, and V5 files from the Xcode project.

--- SundeeFundee/App/DebugSeedData.swift
+++ SundeeFundee/App/DebugSeedData.swift
@@ -93,7 +93,7 @@ enum DebugSeedData {
 
     @MainActor
     static func clearAll(modelContext: ModelContext) {
-        let types: [any PersistentModel.Type] = AppSchemaV2.models
+        let types: [any PersistentModel.Type] = AppSchemaV6.models
         for type in types {
             try? modelContext.delete(model: type)
         }
