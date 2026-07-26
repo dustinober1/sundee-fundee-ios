658d1d2874a33ceeb43be92eec185f05b730760b
dustinober1 <dober.code@gmail.com>
2026-07-26 16:53:02 -0400

    test(checkin): cover daily completion

--- SundeeFundee/Tests/SundeeFundeeKitTests/ViewModelTests/QuickCheckInViewModelTests.swift
+++ SundeeFundee/Tests/SundeeFundeeKitTests/ViewModelTests/QuickCheckInViewModelTests.swift
@@ -6,6 +6,10 @@ final class QuickCheckInViewModelTests: XCTestCase {
     func testSaveSymptomOnlyCheckIn() async {
         let dataClient = MockCloudKitClient()
         let viewModel = QuickCheckInViewModel(dataClient: dataClient)
+        let completionExpectation = expectation(
+            forNotification: .dailyCheckInCompleted,
+            object: nil
+        )
         viewModel.energy = 7
         viewModel.fatigue = 3
         viewModel.soreness = 4
@@ -14,6 +18,7 @@ final class QuickCheckInViewModelTests: XCTestCase {
         await viewModel.save()
 
         XCTAssertNil(viewModel.errorMessage)
+        await fulfillment(of: [completionExpectation], timeout: 0.1)
         XCTAssertEqual(dataClient.recordCount(for: "SymptomCheckInRecord"), 1)
         XCTAssertEqual(dataClient.recordCount(for: "DailyPainLog"), 0)
         XCTAssertEqual(dataClient.recordCount(for: "PeriodLogRecord"), 0)
